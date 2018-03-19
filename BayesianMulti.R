709494505089b41670775af2b70c280c2735f770
Aimee Cheng <aimeechengdev@gmail.com>
2018-03-19 10:43:06 -0700

    prepare beta

--- BayesianMulti.R
+++ BayesianMulti.R
@@ -15,7 +15,7 @@ logprior <- function(beta) {
   return(p)
 }
 
-#load data
+#======   prepare data   ======
 loadData = "MockData"
 idx = 0
 if (loadData == "MockData") {
@@ -84,57 +84,53 @@ if (loadData == "MockData") {
   write.table(data, file = "mockData.txt", sep = ",", row.names = FALSE, col.names = colNames)
 }
 
-
-cols=4;# one intercept + 3 attributes
-rows=10000;
-sigma2=0.05;
-
-datax <- read.csv(file="R-simulated-logist-data-x.csv",head=FALSE,sep=",")
-x1=datax$V1
-x2=datax$V2
-x3=datax$V3
-beta_curr=seq(0.5,cols)/1.0
-beta_new=seq(0.5,cols)/1.0
-datay <- read.csv(file="R-simulated-logist-data-y.csv",head=FALSE,sep=",")
-y=datay$V1
-d1 <- data.frame(1.5, t(beta_curr))                
-write.table(d1, "R-mcmc_logistic_version_2.txt", row.names = FALSE)
-m=5000 # the length of the MCMC samples
-betaSamples =array(0, dim=c(m,cols)) 
-for (i in 1:m)
+#======   sampling beta   ======
+sigmaProposal = 0.05;
+init = seq(0.5, xCols)/1.0
+betaCurr = array(0, dim=c(yCols, xCols)) 
+betaNew = array(0, dim=c(yCols, xCols)) 
+for (j in 1 : yCols) {
+  betaCurr[j,] = init
+  betaNew[j,] = init
+}
+nSamples = 500 # the length of the MCMC samples
+betaSamples =array(0, dim=c(nSamples, yCols, xCols)) 
+for (i in 1 : nSamples)
 {
-  if (i%%500 ==0){
+  if (i %% 50 ==0){
     print(i)
   }
-  for (k in 1:cols)
+  for (k in 1 : xCols)
   {
-    temp1=beta_curr[k] +sigma2*rnorm(1, mean = 0, sd = 1)
-    beta_new[k]=temp1;
-    likelihood=0.0;
+    temp1=betaCurr[k] +sigma2*rnorm(1, mean = 0, sd = 1)
+    betaNew[k]=temp1;
+    logLikelihood=0.0;
     for (h in 1:rows)
     {
-      temp_new=beta_new[1] +beta_new[2]*x1[h] +beta_new[3]*x2[h]+beta_new[4]*x3[h];
+      temp_new=betaNew[1] +betaNew[2]*x1[h] +betaNew[3]*x2[h]+betaNew[4]*x3[h];
       temp_new=exp(temp_new);
-      temp_curr=beta_curr[1] +beta_curr[2]*x1[h] +beta_curr[3]*x2[h]+beta_curr[4]*x3[h];
+      temp_curr=betaCurr[1] +betaCurr[2]*x1[h] +betaCurr[3]*x2[h]+betaCurr[4]*x3[h];
       temp_curr=exp(temp_curr);
-      likelihood=likelihood+ log( temp_new /(1.0 +temp_new))*y[h];
-      likelihood=likelihood+ log(1.0- temp_new /(1.0 +temp_new))*(1.0-y[h]);
-      likelihood=likelihood- log(temp_curr /(1.0 +temp_curr))*y[h];
-      likelihood=likelihood- log(1.0- temp_curr /(1.0 +temp_curr))*(1.0-y[h]);
+      logLikelihood=logLikelihood+ log( temp_new /(1.0 +temp_new))*y[h];
+      logLikelihood=logLikelihood+ log(1.0- temp_new /(1.0 +temp_new))*(1.0-y[h]);
+      logLikelihood=logLikelihood- log(temp_curr /(1.0 +temp_curr))*y[h];
+      logLikelihood=logLikelihood- log(1.0- temp_curr /(1.0 +temp_curr))*(1.0-y[h]);
     }
     
-    likelihood=likelihood + logprior(temp_new)
-    likelihood=likelihood - logprior(temp_curr)
+    logLikelihood=logLikelihood + logprior(temp_new)
+    logLikelihood=logLikelihood - logprior(temp_curr)
     u=runif(1, min = 0, max = 1);
-    if ( u<min(1.0, exp(likelihood)))
+    if ( u<min(1.0, exp(logLikelihood)))
     {
-      beta_curr[k]=beta_new[k];
+      betaCurr[k]=betaNew[k];
     }
   }
-  betaSamples[i,] = beta_curr
-  d1 <- data.frame(likelihood, t(beta_curr))
-  write.table(d1, "R-mcmc_logistic_version_2.txt", row.names = FALSE, col.names = FALSE, append = TRUE)
+  betaSamples[i,] = betaCurr
+  d1 <- data.frame(logLikelihood, t(betaCurr))
+
 }
+
+#======   display beta samples   ======
 b0a = betaSamples[,1]
 plot(b0a, type = "l")
 burnin = 500
